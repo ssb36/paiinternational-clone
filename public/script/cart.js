d56a4effa6d2ef60526176f4e15b632501b508c6
DIVYA-PIDUGU <95955816+PIDUGU-DIVYA@users.noreply.github.com>
2022-02-27 21:47:38 +0530

    working on discount price

--- public/script/cart.js
+++ public/script/cart.js
@@ -123,7 +123,7 @@ function display(cart_items) {
     actual_p += qty*el.price;
     // console.log(el.price, discount_p)
   });
-
+  console.log("actual_p", actual_p);
   displayPrice();
 }
 
@@ -149,8 +149,8 @@ function displayPrice() {
        total_p: total_p,
      }
   let price_data = JSON.parse(localStorage.getItem("pai_price_data"));
-  console.log("dprice", price_data.discount_p);
-  console.log(price_data);
+  // console.log("dprice", price_data.discount_p);
+  // console.log(price_data);
   // console.log("price_data.discount_p);
   if (!price_data.discount_p) {
     localStorage.setItem("pai_price_data", JSON.stringify(p_obj));
